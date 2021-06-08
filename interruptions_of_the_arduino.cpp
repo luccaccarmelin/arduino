27e9387ecd5cc9bddc4b01b20e73afae1870985e
Lucca Vinicius Cardoso Carmelin <81098582+luccaccarmelin@users.noreply.github.com>
2021-06-08 12:35:05 -0300

    Update interruptions_of_the_arduino.cpp

--- interruptions_of_the_arduino.cpp
+++ interruptions_of_the_arduino.cpp
@@ -1,21 +1,41 @@
-int LED_PRENSA=4;
+int LED_PRENSA_BRANCO=4;
+int LED_PRENSA_AMARELO=5;
 int CHAVE=2;
+int CHAVE_ONOFF=3;
 
-  void setup()
+   void setup()
  {
-  pinMode(LED_PRENSA,OUTPUT);
+  pinMode(LED_PRENSA_BRANCO,OUTPUT);
+  pinMode(LED_PRENSA_AMARELO,OUTPUT);
   pinMode(CHAVE,INPUT_PULLUP);
+  pinMode(CHAVE_ONOFF,INPUT_PULLUP);
   attachInterrupt(0,paraPrensa,LOW);
  }
 
-  void loop()
+   void loop()
  {
-  digitalWrite(LED_PRENSA,HIGH);
-  delay(5000);
+     if(digitalRead(CHAVE_ONOFF)==0)
+ {
+  digitalWrite(LED_PRENSA_BRANCO,HIGH);
+  digitalWrite(LED_PRENSA_AMARELO,LOW);
+  
+  delay(1000);
+  
+  digitalWrite(LED_PRENSA_BRANCO,LOW);
+  digitalWrite(LED_PRENSA_AMARELO,HIGH);
+  
+  delay(1000);
+ }
+   else
+ {
+  digitalWrite(LED_PRENSA_BRANCO,LOW);
+  digitalWrite(LED_PRENSA_AMARELO,LOW);
+ }
 
  }
 
-  void paraPrensa()
+   void paraPrensa()
  {
-  digitalWrite(LED_PRENSA,LOW);
+  digitalWrite(LED_PRENSA_BRANCO,LOW);
+  digitalWrite(LED_PRENSA_AMARELO,LOW);
  }
